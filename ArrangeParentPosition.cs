4b33584a22f6829e36e43a049dc61552742bf833
Adil Emre bilaloğlu <44676177+aemrebilaloglu@users.noreply.github.com>
2023-03-01 22:15:53 +0300

    Add files via upload

---
+++ ArrangeParentPosition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrangeParentPosition : MonoBehaviour
+{
+    private void ArrangeParentPos()
+    {
+        Vector3 centerPos = Vector3.zero;
+        List<Transform> children = new List<Transform>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            centerPos += transform.GetChild(i).position;
+            children.Add(transform.GetChild(i));
+        }
+        centerPos /= transform.childCount;
+        transform.DetachChildren();
+        transform.position = centerPos;
+        for (int i = 0; i < children.Count; i++)
+        {
+            children[i].SetParent(transform);
+        }
+    }
+}
